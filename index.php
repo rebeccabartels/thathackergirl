6972a3c56d0267e68f9a74812e26255bfe02572b
riley <rebeccakbartels@gmail.com>
2020-01-29 20:24:41 -0500

    test

---
+++ index.php
@@ -0,0 +1,453 @@
+<!DOCTYPE html>
+<!-- saved from url=(0024)https://rebeccabartels.github.io/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script type="text/javascript" async="" src="./ThatHackerGirl/analytics.js"></script><script async="" src="./ThatHackerGirl/js"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-155347973-1');
+	</script>
+	<!-- meta -->
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Rebecca Bartels - @ThatHackerGirl</title>
+
+	<!-- tab img -->
+	<link rel="icon" href="./ThatHackerGirl/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./ThatHackerGirl/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./ThatHackerGirl/favicon.ico">
+
+	<!-- css plugin -->
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/flexslider.css">
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/aos.css">
+
+	<!-- main css -->
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/style.css">
+	<link rel="stylesheet" href="./ThatHackerGirl/assets/css/style-responsives.css">
+
+	<!-- google fonts -->
+	<link href="./ThatHackerGirl/assets/css/style.css" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style>@media print {#ghostery-purple-box {display:none !important}}</style>
+</head>
+
+<body data-aos-easing="ease" data-aos-duration="1200" data-aos-delay="0"><div style="position:fixed;top:0px;left:0px;width:0;height:0;" id="scrollzipPoint"></div>
+
+	<!-- // PRELOADER BEGIN -->
+	<div id="preloader" style="display: none;">
+		<div id="status" style="display: none;">
+			<div class="loader-revolve center">
+				<span></span>
+			</div>
+		</div>
+	</div>
+	<!-- // PRELOADER END -->
+
+	<!-- // NAVIGATION BEGIN -->
+	<div id="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="logo">
+						<a class="smoth-scroll" href="https://rebeccabartels.github.io/#home">Rebecca.</a>
+					</div>
+				</div>
+
+				<div class="col-sm-9">
+					<div class="navigation-menu">
+						<div class="navbar">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<div class="navbar-collapse collapse">
+								<ul class="nav navbar-nav navbar-right">
+									<li><a class="smoth-scroll" href="https://rebeccabartels.github.io/#about">About</a></li>
+									<li><a class="smoth-scroll" href="https://rebeccabartels.github.io/#services">Skills</a></li>
+									<li><a class="smoth-scroll" href="https://rebeccabartels.github.io/#portfolio">Certifications</a></li>
+									<li><a class="smoth-scroll" href="https://rebeccabartels.github.io/#contact">Contact</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // NAVIGATION END -->
+
+	<!-- // HOME BEGIN -->
+	<div id="home" class="home-bg" data-stellar-background-ratio="0.6" style="background-position: 0px 0px;">
+		<div class="overlay">
+			<div class="container">
+				<div class="row">
+					<div class="home-text">
+						<div class="profile-pic aos-init aos-animate" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0">
+							<img src="./ThatHackerGirl/avatar.png">
+						</div>
+
+						<div data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0" class="aos-init aos-animate">
+							<h1>Hi, I'm Rebecca Bartels</h1>
+							<h3 id="text-rotator">
+								<div style="display: none;">Offensive Security Enthusiast</div>
+								<div style="display: none;">Software Engineer</div>
+								<div style="display: block; opacity: 0.684062;">Data Analyist</div>
+							</h3>
+						</div>
+					</div>
+					<a class="smoth-scroll" href="https://rebeccabartels.github.io/#about"><i class="fa fa-angle-down arrow lift"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // HOME END -->
+
+	<!-- // ABOUT BEGIN -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="description col-md-6 col-sm-4 col-xs-12">
+					<h2>Bio</h2>
+					<p>I have a proven background in software engineering,
+					<br>system security, pentesting, and data analytics. 
+					</p>
+
+					<ul class="social-icons">
+						<li><a href="https://github.com/rebeccabartels" target="_blank"><i class="fa fa-github"></i></a></li>
+						<li><a href="https://twitter.com/thathackergirl" target="_blank"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/rebecca-bartels-50665747/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+
+					<p>
+						<a href="rebeccabartels.github.io/Rebecca_Bartels_Resume.pdf" target="_blank" class="btn btn-dark with-arrow smoth-scroll">My Resume
+							<i class="fa fa-download"></i>
+						</a>
+					</p>
+				</div>
+
+				<div class="facts col-md-5 col-sm-5 col-xs-12">
+					<div class="row">
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller scrollzip roller-title-number-1" data-slno="1" data-min="0" data-max="300" data-delay="10" data-increment="5">5</div>
+								<div class="count-name-intro">Levels of HacktheBox</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller roller-title-number-2 scrollzip" data-slno="2" data-min="0" data-max="1500" data-delay="10" data-increment="5">1500</div>
+								<div class="count-name-intro">Cups of coffe</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller roller-title-number-3 scrollzip" data-slno="3" data-min="0" data-max="120" data-delay="10" data-increment="5">4</div>
+								<div class="count-name-intro">Bootcamps/Certifications</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<div class="count-item">
+								<div class="numscroller roller-title-number-4 scrollzip" data-slno="4" data-min="0" data-max="10" data-delay="1" data-increment="1">7</div>
+								<div class="count-name-intro">Years of Software Engineering Experience</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // ABOUT END -->
+
+	<!-- // SERVICES BEGIN -->
+	<section id="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Skills</h2>
+				</div>
+
+				<div class="col-md-12">
+					<div class="row">
+
+						<div class="col-md-4 col-sm-6">
+							<div class="services-item">
+								<i class="fa fa-code fa-2x"></i>
+								<h4>Development Background</h4>
+								<p>I  have been coding since I was about 8 years old.  Since then, I have mastered both front and back end development for web and mobile applications.</p>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6">
+							<div class="services-item">
+								<i class="fa fa-server fa-2x"></i>
+								<h4>Efficient Automation</h4>
+								<p>As a sys admin, I learned how important automations are for a business. Building, maintaining, and deploying algorithms can positively change one's business.</p>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6">
+							<div class="services-item">
+								<i class="fa fa-shield fa-2x"></i>
+								<h4>Aspiring Ethical Hacker</h4>
+								<p>In 2019, I attended Columbia University's Cyber Security Bootcamp in New York City.  There, I mastered fundamental security measures for code and learned how to mitigate vulnerabilities and fires.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // SERVICES END -->
+
+	<!-- // PORTFOLIO BEGIN -->
+	<section id="portfolio">
+		<div class="container">
+			<div class="row">
+				<div data-aos="fade-up" class="aos-init">
+					<div class="col-md-12 text-center">
+						<h2>Education & Certifications</h2>
+					</div>
+
+					<div class="col-md-12 col-sm-12">
+						<div class="filter-btn col-md-12 text-center">
+							<div class="filter active" data-filter="all">All</div>
+							<div class="filter" data-filter=".completed">Completed</div>
+							<div class="filter" data-filter=".ongoing">Ongoing</div>
+							<div class="filter lst-cld" data-filter=".upcoming">Upcoming</div>
+						</div>
+
+						<div id="port-image" class="container">
+							<div class="row">
+
+								<div class="completed grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://aws.amazon.com/certification/certified-cloud-practitioner/" target="_blank">
+										<figure class="port-desc">
+											<img src="./ThatHackerGirl/certificate-1.png" class="img-responsive" alt="Work 1">
+											<figcaption>
+												<h4>Web Development</h4>
+												<h5>Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="completed grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://flatironschool.com/" target="_blank">
+										<figure class="port-desc">
+											<img src="./ThatHackerGirl/certificate-2.png" class="img-responsive" alt="Work 2">
+											<figcaption>
+												<h4>iOS Development</h4>
+												<h5>Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="ongoing grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://aws.amazon.com/certification/certified-sysops-admin-associate/" target="_blank">
+										<figure class="port-desc">
+											<img src="./ThatHackerGirl/certificate-3.png" class="img-responsive" alt="Work 3">
+											<figcaption>
+												<h4>Cybersecurity & Ethical Hacking Bootcamp</h4>
+												<h5>Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="completed grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://aws.amazon.com/certification/certified-solutions-architect-professional/" target="_blank">
+										<figure class="port-desc">
+											<img src="./ThatHackerGirl/certificate-4.png" class="img-responsive" alt="Work 4">
+											<figcaption>
+												<h4>Salesforce Administration Certification Course I</h4>
+												<h5>Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="upcoming grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="https://aws.amazon.com/certification/certified-devops-engineer-professional/" target="_blank">
+										<figure class="port-desc">
+											<img src="./ThatHackerGirl/certificate-5.png" class="img-responsive" alt="Work 5">
+											<figcaption>
+												<h4>Security Plus +</h4>
+												<h5>Upcoming</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+								<div class="ongoing grid mix col-md-4 col-sm-6 col-xs-12" style="display: inline-block;">
+									<a href="./ThatHackerGirl/certificate-6.png" class="image-popup">
+										<figure class="port-desc">
+											<img src="./ThatHackerGirl/certificate-6.png" class="img-responsive" alt="Work 6">
+											<figcaption>
+												<h4>HacktheBoxEU</h4>
+												<h5>Lessons/Bounties Completed</h5>
+											</figcaption>
+										</figure>
+									</a>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // PORTFOLIO END -->
+
+	<!-- // TESTIMONIAL BEGIN -->
+	<section id="testimonial" class="testimonial-slider text-center">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="flexslider">
+						<ul class="slides">
+
+							<li class="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;">
+								<p class="author">Linus Torvalds</p>
+								<h2>“Talk is cheap. Show me the code.” <br> github: rebeccabartels </h2>
+							</li>
+
+							<li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;" class="">
+								<!-- <p class="author">Unknown</p> -->
+								<h2>“Servers? Where we’re going we don’t need servers.”</h2>
+							</li>
+
+							<li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;" class="flex-active-slide">
+								<p class="author">Stephen Hawking</p>
+								<h2>“I am just a child who has never grown up. I still keep asking these 'how' and 'why' questions. Occasionally, I find an answer.”</h2>
+							</li>
+
+						</ul>
+					<ol class="flex-control-nav flex-control-paging"><li><a class="">1</a></li><li><a class="">2</a></li><li><a class="flex-active">3</a></li></ol></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // TESTIMONIAL END -->
+
+	<!-- // CONTACT BEGIN -->
+	<section id="contact">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h2>Contact Me</h2>
+				</div>
+
+				<div class="address-icons col-md-8 col-md-offset-2">
+					<div class="col-md-4 col-sm-4">
+						<i class="fa fa-envelope-o fa-2x"></i>
+						<p>beccacode@gmail.com</p>
+					</div>
+
+					<div class="col-md-4 col-sm-4">
+						<!-- <i class="fa fa-map-marker fa-2x"></i>
+						<p>Banguntapan,
+							<br> Yogyakarta, ID 55198
+						</p> -->
+					</div>
+
+					<div class="col-md-4 col-sm-4">
+						<i class="fa fa-whatsapp fa-2x"></i>
+						<p>2038364189</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+
+				<div class="col-md-8 col-md-offset-2">
+					<!-- changes your email domain in mailer.php (assets/php/) -->
+					<form id="ajax-contact" name="contact" method="post" action="assets/php/">
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<div class="form-group">
+									<input type="text" name="name" id="name" class="form-control" placeholder="Name" required="">
+									<p class="help-block text-danger"></p>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6">
+								<div class="form-group">
+									<input type="email" name="email" id="email" class="form-control" placeholder="Email" required="">
+									<p class="help-block text-danger"></p>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<textarea name="message" id="message" class="form-control" rows="4" placeholder="Message..." required=""></textarea>
+							<p class="help-block text-danger"></p>
+						</div>
+						<div class="col-md-12 col-xs-12">
+							<button id="submit" type="submit" name="submit" class="btn btn-dark">Send Message</button>
+						</div>
+						<div id="form-messages" class="col-md-12 col-xs-12"></div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // CONTACT END-->
+
+	<!-- // FOOTER BEGIN -->
+	<footer id="footer">
+		<div class="container">
+			<div class="col-md-12 text-center">
+				<ul class="social-icons-footer">
+					<li><a href="https://github.com/rebeccabartels/" target="_blank"><i class="fa fa-github"></i></a></li>
+					<li><a href="https://twitter.com/thathackergirl" target="_blank"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/rebecca-bartels-50665747/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+			</ul>
+
+				<p>© 2020</p>
+			</div>
+		</div>
+	</footer>
+	<!-- // FOOTER END -->
+
+
+
+
+	<!-- js plugin -->
+	<script src="./ThatHackerGirl/jquery.js"></script>
+	<script src="./ThatHackerGirl/bootstrap.min.js"></script>
+	<script src="./ThatHackerGirl/jquery.stellar.min.js"></script>
+	<script src="./ThatHackerGirl/text-rotate.js"></script>
+	<script src="./ThatHackerGirl/numscroller.js"></script>
+	<script src="./ThatHackerGirl/jquery.appear.js"></script>
+	<script src="./ThatHackerGirl/progress-bar.min.js"></script>
+	<script src="./ThatHackerGirl/jquery.mixitup.js"></script>
+	<script src="./ThatHackerGirl/jquery.magnific-popup.min.js"></script>
+	<script src="./ThatHackerGirl/magnific-popup-options.js"></script>
+	<script src="./ThatHackerGirl/jquery.flexslider-min.js"></script>
+	<script src="./ThatHackerGirl/aos.js"></script>
+	<script src="./ThatHackerGirl/app.js"></script>
+
+	<!-- main js -->
+	<script src="./ThatHackerGirl/main.js"></script>
+
+
+</body></html>
